08840dc17d4c94da34df48aa45adb0be559b018c
Till <quastus@gmail.com>
2019-02-11 20:44:48 +0100

    Update InfluxArduino.cpp

--- InfluxArduino.cpp
+++ InfluxArduino.cpp
@@ -2,22 +2,25 @@
 #include "InfluxArduino.hpp"
 
 InfluxArduino::InfluxArduino()
-{
+{ 
+    
 }
 
 InfluxArduino::~InfluxArduino()
 {
+    
 }
 
 void InfluxArduino::configure(const char database[],const char host[],const uint16_t port)
 {   
     //copy these strings to private class pointers for future use
-
+    
     _database = new char[strlen(database)+1];
     strcpy(_database,database); //strncpy fails for some reason
     _host = new char[strlen(host)+1];
     strcpy(_host,host);
     _port = port;
+    
 }
 
 void InfluxArduino::addCertificate(const char cert[])
@@ -43,15 +46,18 @@ void InfluxArduino::authorize(const char username[], const char password[])
 
 bool InfluxArduino::write(const char *measurement,const char *fieldString)
 {
+    yield();  // reset esp watchdog
     write(measurement,"",fieldString);
+    
 }
 
 bool InfluxArduino::write(const char *measurement,const char *tagString,const char *fieldString)
 {   
+    yield();  // reset esp watchdog
     HTTPClient http;
     char uri[32];
     sprintf(uri, "/write?db=%s", _database);
-
+    
     if(_isSecure)
     {
        http.begin(_host, _port, uri, _cert);
@@ -66,7 +72,7 @@ bool InfluxArduino::write(const char *measurement,const char *tagString,const ch
     {
         http.setAuthorization(_username,_password);
     }
-
+    
     char writeBuf[512]; // ¯\_(ツ)_/¯ 
     if(strlen(tagString) > 0)
     {
@@ -79,17 +85,24 @@ bool InfluxArduino::write(const char *measurement,const char *tagString,const ch
         sprintf(writeBuf,"%s %s",measurement,fieldString); //no comma between tags and fields
     }
     Serial.println(writeBuf);
+    
     _latestResponse = http.POST(writeBuf);
     http.end();
+    yield();  // reset esp watchdog
     return _latestResponse == 204;
+    
 }
 
 int InfluxArduino::getResponse()
 {
+    yield();  // reset esp watchdog
     return _latestResponse;
+    
 }
 
 bool InfluxArduino::isSecure()
 {
+    yield();  // reset esp watchdog
     return _isSecure;
+    
 }
